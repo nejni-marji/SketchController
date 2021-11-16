f74a117ee8d43fcb39f2cb5e5687a323083de503
nejni-marji <nejni.marji@gmail.com>
2021-11-16 13:06:35 -0600

    release mouse buttons before exiting the program

--- sketchcontroller.cpp
+++ sketchcontroller.cpp
@@ -13,5 +13,9 @@ int main() {
 	threadPollGamepad.join();
 	threadQueryGamepad.join();
 
+	// release mouse, just in case
+	inputData.dispCur.sendClick(1, false);
+	inputData.dispCur.sendClick(3, false);
+
 	return 0;
 }
